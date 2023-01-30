59a0e0fa411e0e5ca5cd29a90b209e40bee96ac6
michael <michaelsanchez8815@gmail.com>
2023-01-30 00:11:12 -0600

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        // Entry point of our program
+        static void Main(string[] args)
+        {
+            // Console.ForegroundColor = ConsoleColor.DarkYellow;
+            // Console.BackgroundColor = ConsoleColor.DarkGray;
+            // Console.Clear();
+
+            Console.WriteLine("Hello Michael");
+            Console.WriteLine("C# is cool!");
+            Console.Read();
+            /* Console.Write("Enter a string and press enter: ");
+            string readInput = Console.ReadLine();
+            Console.WriteLine("You have entered {0}", readInput);
+
+            Console.Write("Enter a string and press enter: ");
+            int asciiValue = Console.Read();
+            Console.WriteLine("ASCII values is {0}", asciiValue);
+            Console.ReadKey();*/
+            // string basics
+            /*string myName = "Michael";
+
+            string message = "My name is " + myName;
+
+            string capsMessage = message.ToUpper();
+
+            string lowerCaseMessage = capsMessage.ToLower();
+
+            Console.WriteLine(message);
+            Console.WriteLine(capsMessage);
+            Console.WriteLine(lowerCaseMessage);
+            Console.Read();
+
+
+            // declaring multiple variables at once
+            int num3, num4, num5;
+
+            // declaring a variable
+            int num1;
+            // assigning a value to a variable
+            num1 = 13;
+
+            // declaring and initializing a variabe
+            int num2 = 23;
+            long myLongNum = 321651561351351;
+
+            num2 = 100;
+            // Using Concatination
+            Console.WriteLine("num is: " + num1);
+            int sum = num1 + num2;
+            Console.WriteLine("num1: " + num1 + " num2: "+ num2 + " is " + sum);
+
+            double d1 = 3.1415;
+            double d2 = 5.1;
+            double dDive = d1 / d2;
+            Console.WriteLine("d1/d2 is: " + dDive);
+
+
+            float f1 = 3.1415f;
+            float f2 = 5.1f;
+            float fDiv = f1 / f2;
+            Console.WriteLine("f1/f2 is: "+ fDiv);
+
+            double dIDiv = d1 / num1;
+            Console.WriteLine("d1/num1 is: " + dIDiv);
+            Console.Read();*/
+        }
+    }
+}
