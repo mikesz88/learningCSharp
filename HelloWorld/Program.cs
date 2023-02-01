dc7b784e2301c50d45ab7f0c0af9f175fa2e1f5a
michael <michaelsanchez8815@gmail.com>
2023-02-01 12:45:52 -0600

    data type exercise

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -36,7 +36,7 @@
             Console.WriteLine(myName.Trim());
             Console.WriteLine(myName.Substring(3));*/
 
-            Console.Write("Enter a string here: ");
+            /*Console.Write("Enter a string here: ");
             string newString = Console.ReadLine();
             Console.Write("Enter the character to search: ");
             char newChar = Console.ReadLine()[0];
@@ -47,7 +47,41 @@
 
             Console.Write("Enter your last name:");
             string lastName = Console.ReadLine();
-            Console.WriteLine($"{firstName} {lastName}");
+            Console.WriteLine($"{firstName} {lastName}");*/
+
+            byte smallNumber = 100;
+            Console.WriteLine(smallNumber);
+            sbyte sbyteNumber = -100;
+            Console.WriteLine(sbyteNumber);
+            int integer = 100000;
+            Console.WriteLine(integer);
+            uint unsignedInt = 4000000;
+            Console.WriteLine(unsignedInt);
+            short shortNum = -32000;
+            Console.WriteLine(shortNum);
+            ushort unsignedShortNum = 60000;
+            Console.WriteLine(unsignedShortNum);
+            long longNum = 9223372036854775807;
+            Console.WriteLine(longNum);
+            ulong unsignedLongNum = 18446744073709551615;
+            Console.WriteLine(unsignedLongNum);
+            float floatNum = -3.402823f;
+            Console.WriteLine(floatNum);
+            double doubleNum = 1.79769313486232;
+            Console.WriteLine(doubleNum);
+            char character = 'a';
+            Console.WriteLine(character);
+            bool boolNum = false;
+            Console.WriteLine(boolNum);
+            string stringTest = "test";
+            Console.WriteLine(stringTest);
+            decimal decimalNum = 1.5468687248725986752967529867259867568789567859678259067589m;
+            Console.WriteLine(decimalNum);
+
+            string string1 = "I control text";
+            Console.WriteLine(string1);
+            string string2 = "12";
+            Console.WriteLine(int.Parse(string2));
 
 
         }
