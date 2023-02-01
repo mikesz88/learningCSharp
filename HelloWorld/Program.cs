1743f8874f9a3e595c647486ad7e1b0ccb783661
michael <michaelsanchez8815@gmail.com>
2023-02-01 14:48:48 -0600

    returning methods with parameters

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,19 +4,27 @@
     {
         static void Main(string[] args)
         {
-            WriteSomething();
-            WriteSomethingSpecific("I am an Argument and am called from a method");
-        }
-        // access modifier (static) return type method name (parameter1, parameter2)
-        public static void WriteSomething() 
-        {
-            Console.WriteLine("I am called from a method");
-        }
-
-        public static void WriteSomethingSpecific(string myText) 
-        {
-            Console.WriteLine(myText);
+            Console.WriteLine(Add(Add(1,2), Add(3, 4)));
+            Console.WriteLine(Add(15, 31));
+            Console.WriteLine(Multiply(5,3));
+            Console.WriteLine(Divide(12,5));
             Console.Read();
         }
+        
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double Divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
     }
 }
