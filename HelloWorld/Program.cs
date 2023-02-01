a4361020251d04da3b786b9ab724f887c9a8aa4b
michael <michaelsanchez8815@gmail.com>
2023-01-31 22:51:31 -0600

    naming convention rules

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,72 +1,31 @@
 ﻿namespace HelloWorld
 {
-    internal class Program
+    // Pascal Case that are nouns
+    internal class ClientActivity
     {
         // Entry point of our program
+        // Method names like CalculateValue
+        // method args like firstNumber
         static void Main(string[] args)
         {
-            // Console.ForegroundColor = ConsoleColor.DarkYellow;
-            // Console.BackgroundColor = ConsoleColor.DarkGray;
-            // Console.Clear();
+            // local variables like itemCount
 
-            Console.WriteLine("Hello Michael");
-            Console.WriteLine("C# is cool!");
-            Console.Read();
-            /* Console.Write("Enter a string and press enter: ");
-            string readInput = Console.ReadLine();
-            Console.WriteLine("You have entered {0}", readInput);
+            // use userControl instead of usrCtr
 
-            Console.Write("Enter a string and press enter: ");
-            int asciiValue = Console.Read();
-            Console.WriteLine("ASCII values is {0}", asciiValue);
-            Console.ReadKey();*/
-            // string basics
-            /*string myName = "Michael";
+            // don't use numbers at the start of variable names
+            // avoid underscore
 
-            string message = "My name is " + myName;
+            /*
+                Correct: 
+                string myName;
+                int lastNum;
+                bool isSaved;
+            */
 
-            string capsMessage = message.ToUpper();
-
-            string lowerCaseMessage = capsMessage.ToLower();
-
-            Console.WriteLine(message);
-            Console.WriteLine(capsMessage);
-            Console.WriteLine(lowerCaseMessage);
-            Console.Read();
-
-
-            // declaring multiple variables at once
-            int num3, num4, num5;
-
-            // declaring a variable
-            int num1;
-            // assigning a value to a variable
-            num1 = 13;
-
-            // declaring and initializing a variabe
-            int num2 = 23;
-            long myLongNum = 321651561351351;
-
-            num2 = 100;
-            // Using Concatination
-            Console.WriteLine("num is: " + num1);
-            int sum = num1 + num2;
-            Console.WriteLine("num1: " + num1 + " num2: "+ num2 + " is " + sum);
-
-            double d1 = 3.1415;
-            double d2 = 5.1;
-            double dDive = d1 / d2;
-            Console.WriteLine("d1/d2 is: " + dDive);
-
-
-            float f1 = 3.1415f;
-            float f2 = 5.1f;
-            float fDiv = f1 / f2;
-            Console.WriteLine("f1/f2 is: "+ fDiv);
-
-            double dIDiv = d1 / num1;
-            Console.WriteLine("d1/num1 is: " + dIDiv);
-            Console.Read();*/
+            // Avoid
+            // String myName;
+            // Int32 lastNum;
+            // Boolean isSaved;
         }
     }
 }
