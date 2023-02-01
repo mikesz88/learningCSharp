62a1872c631b8aafff890e3c97afb848523694cd
michael <michaelsanchez8815@gmail.com>
2023-02-01 14:37:30 -0600

    void methods

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,19 +1,22 @@
 ﻿namespace ConstantsC
 {
-        // Constants are immutable values which are known
-        // at compile time and do not change for the life of the program.
     internal class Program
     {
-        // constants fields
-        const double PI = 3.14159265359;
-        const int weeks = 52, months = 12;
-        // Create a constant of type string with your birthday as its value
-        const string myBirthday = "07/28/1988";
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine($"My birthday is always going to be {myBirthday}");
-            Console.Read();
+            WriteSomething();
+            WriteSomethingSpecific("I am an Argument and am called from a method");
+        }
+        // access modifier (static) return type method name (parameter1, parameter2)
+        public static void WriteSomething() 
+        {
+            Console.WriteLine("I am called from a method");
+        }
 
+        public static void WriteSomethingSpecific(string myText) 
+        {
+            Console.WriteLine(myText);
+            Console.Read();
         }
     }
 }
