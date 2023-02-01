718fd76862c8a993351ab2832b14eb51adabb89e
michael <michaelsanchez8815@gmail.com>
2023-01-31 23:03:43 -0600

    parse strings into Int

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,28 +5,13 @@
 
         static void Main(string[] args)
         {
-            // implicit conversion
-            int num = 12390532;
-            long bigNum = num;
-
-            float myFloat = 13.37f;
-            double myNewDouble = myFloat;
-
-            double myDouble = 13.37;
-            int myInt;
-            // explicit conversion
-            // cast double to int;
-            myInt = (int)myDouble;
-
-            // typeConversion
-            string myString = myDouble.ToString(); // "13.37"
-            string myStringFloat = myFloat.ToString();
-            bool sunIsShining = false;
-            string myBoolString = sunIsShining.ToString();
-
-            Console.WriteLine(myString);
-            Console.WriteLine(myStringFloat);
-            Console.WriteLine(myBoolString);
+            string myString = "15";
+            string mySecondString = "13";
+            int num1 = Int32.Parse(myString);
+            int num2 = Int32.Parse(mySecondString);
+            int resultInt = num1 + num2;
+            string result = myString + mySecondString;
+            Console.WriteLine(resultInt);
             Console.Read();
         }
     }
