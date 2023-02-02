6a6a0ac86ab40e36a832a9f0f4faa0e28249475e
michael <michaelsanchez8815@gmail.com>
2023-02-02 10:09:06 -0600

    method/function exercise

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,28 +2,21 @@
 {
     internal class Program
     {
+        const string name1 = "Michael";
+        const string name2 = "Lisa";
+        const string name3 = "Lily";
+
         static void Main(string[] args)
         {
-            Console.WriteLine(Add(Add(1,2), Add(3, 4)));
-            Console.WriteLine(Add(15, 31));
-            Console.WriteLine(Multiply(5,3));
-            Console.WriteLine(Divide(12,5));
+            GreetFriend(name1);
+            GreetFriend(name2);
+            GreetFriend(name3);
             Console.Read();
         }
-        
-        public static int Add(int num1, int num2)
-        {
-            return num1 + num2;
-        }
 
-        public static int Multiply(int num1, int num2)
+        public static void GreetFriend(string name)
         {
-            return num1 * num2;
-        }
-
-        public static double Divide(double num1, double num2)
-        {
-            return num1 / num2;
+            Console.WriteLine($"Hi {name}, my friend!");
         }
 
     }
