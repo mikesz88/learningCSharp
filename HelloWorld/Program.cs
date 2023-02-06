16251535c92e3603f459574808d4eeb2e587a0f7
michael <michaelsanchez8815@gmail.com>
2023-02-06 11:45:42 -0600

    attempted login

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,34 +2,54 @@
 {
     public class Exercise
     {
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Please provide a number");
-            string stringAsNumber = Console.ReadLine()!;
-            int num = int.Parse(stringAsNumber);
-            NestedCheck(num);
-        }
-        static void NestedCheck(int number)
-        {
-            if (number % 3 == 0)
+            string username, password;
+            string[] credentials = new string[2];
+            credentials = registerUser();
+
+            username= credentials[0];
+            password= credentials[1];
+
+            bool loginStatus = loginUser(username, password);
+            
+            if (loginStatus)
             {
-                Console.WriteLine("Divisible by 3.");
-            } 
-            else if (number % 7 == 0)
+                Console.WriteLine("You are logged in");
+            }
+            else
             {
-                Console.WriteLine("Divisible by 7.");
-            } 
-            else if (number % 2 == 1)
-            {
-                Console.WriteLine("Odd number.");
-            } 
-            else 
-            {
-                Console.WriteLine("Even number.");
+                Console.WriteLine("Invalid Username and Password");
             }
 
 
+
+        }
+
+        static string[] registerUser()
+        {
+            Console.WriteLine("What username would you like?");
+            string username = Console.ReadLine()!;
+            Console.WriteLine("What password would you like?");
+            string password = Console.ReadLine()!;
+            string[] credentials = new string[2] { username, password };
+            return credentials;
+        }
+
+        static bool loginUser(string username, string password)
+        {
+            Console.WriteLine("Please login by writing your username and password ");
+            Console.WriteLine("Username: ");
+            string attemptedUsername = Console.ReadLine()!;
+            Console.WriteLine("Password: ");
+            string attemptedPassword = Console.ReadLine()!;
+            if (attemptedUsername.Equals(username) && attemptedPassword.Equals(password))
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
         }
 
     }
