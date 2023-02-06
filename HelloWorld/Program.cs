8dcb9f5687a81cc974894452cee2fe847325a1e7
michael <michaelsanchez8815@gmail.com>
2023-02-05 21:39:44 -0600

    operators

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,43 +6,76 @@
         static void Main(string[] args)
         {
             int num1 = 5;
-            int num2 = 0;
-            int result;
+            int num2 = 3;
+            int num3;
 
-            try
-            {
-                result = num1 / num2;
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("Cannot divide by 0");
-            }
-            finally
-            {
-                Console.WriteLine("This is called anyways!");
-            }
-            /*Console.WriteLine("Please enter a number!");
-            string input = Console.ReadLine()!;
-            try
-            {
-                int userInputAsInt = int.Parse(input);
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Format exception, please enter the correct type next time.");
-            }
-            catch (OverflowException)
-            {
-                Console.WriteLine("Overflow exception, the number was too long or too shor for an int32.");
-            }
-            catch (ArgumentNullException)
-            {
-                Console.WriteLine("ArgumentNullException, ThreadExceptionEventArgs values was empty (Null)");
-            } 
-            finally
-            {
-                Console.WriteLine("This is called anyways!");
-            };*/
+
+            // unary operator
+            num3 = -num1; // adding a '-' sign multiplies it by -1.
+            Console.WriteLine($"num3 is {num3}");
+
+
+            bool isSunny = true;
+            Console.WriteLine($"Is it sunny? {!isSunny}"); // Adding an exclamation gives opposite value.
+
+            // increment operators
+            int num = 0;
+            num++; // increases it by one.
+            Console.WriteLine($"num is {num}");
+            Console.WriteLine($"num is {num++}"); // This is does not change it on the current line. Only after it because ++ is after the variable. 
+            // post increment is what it is called. 
+            Console.WriteLine($"num is {++num}"); // This changes it prior to calling it because ++ is before the variable
+            // pre increment is what it is called.
+
+            // decrement operator
+            num--;
+            Console.WriteLine($"num is {num}");
+            Console.WriteLine($"num is {num--}"); 
+            Console.WriteLine($"num is {--num}");
+
+            // Math operators
+            int result;
+            result = num1 + num2;
+            Console.WriteLine($"result of num1 + num2 is: {result}");
+
+            result = num1 - num2;
+            Console.WriteLine($"result of num1 - num2 is: {result}");
+
+            result = num1 * num2;
+            Console.WriteLine($"result of num1 * num2 is: {result}");
+
+            result = num1 / num2;
+            Console.WriteLine($"result of num1 / num2 is: {result}");
+
+            result = num1 % num2;
+            Console.WriteLine($"result of num1 % num2 is: {result}");
+
+            // relational and type operators
+            bool isLower;
+            isLower = num1 < num2;
+            Console.WriteLine($"result of num1 < num2 is: {isLower}");
+
+            isLower = num1 > num2;
+            Console.WriteLine($"result of num1 > num2 is: {isLower}");
+
+            // equality operator
+            bool isEqual;
+            isEqual = num1 == num2;
+            Console.WriteLine($"result of num1 == num2 is: {isEqual}");
+
+            isEqual = num1 != num2;
+            Console.WriteLine($"result of num1 != num2 is: {isEqual}");
+
+            // Conditional operators
+            bool isLowerAndSunny;
+            // condition 1 AND condition 2
+            isLowerAndSunny = isLower && isSunny;
+            Console.WriteLine($"result of isLower && isSunny is: {isLowerAndSunny}");
+
+            // condition 1 OR condition 2
+            isLowerAndSunny = isLower || isSunny;
+            Console.WriteLine($"result of isLower || isSunny is: {isLowerAndSunny}");
+
 
             Console.ReadKey();
         }
