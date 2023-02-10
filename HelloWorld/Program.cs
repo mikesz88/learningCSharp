fccae1c3c44560d77c2cf5d383c54d45a617d8af
michael <michaelsanchez8815@gmail.com>
2023-02-10 10:39:18 -0600

    for loop practice

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,42 +1,19 @@
-﻿namespace EnhancedIfStatement
+﻿namespace ForLoop
 {
     public class Exercise
     {
         static void Main(string[] args)
         {
-            // condition ? first_expression : second_expression;
-            // condition has to be either true or false
-            // The conditional operator is right - associative.
-            // The expression a ? b : c ? d : e
-            // is evaluated as a ? b : (c ? d : e),
-            // not as (a ? b : c ) ? d : e.
-            // The conditional operator cannot be overloaded.
-
-            int temperature;
-            Console.WriteLine("Please write the temperature");
-            string tempString = Console.ReadLine()!;
-            bool isANum = int.TryParse(tempString, out temperature);
-            if (isANum)
+            for(int counter = 0; counter <= 20; counter++)
             {
-                checkTemp(temperature);
+                if (counter % 2 == 1)
+                {
+                    Console.WriteLine(counter);
+                }
             }
-            else
-            {
-                Console.WriteLine("Not a valid Temperature");
-            }
+            Console.WriteLine("For loop is done");
             Console.ReadKey();
         }
-
-        static void checkTemp(int temperature)
-        {
-            string message = temperature <= 15 
-                ? "It is too cold here"
-                : temperature > 28
-                ? "It is too hot here"
-                : "It is ok";
-            Console.WriteLine(message);
-        }
-
 
 
     }
