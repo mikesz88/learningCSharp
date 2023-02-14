17ab166fbb2296f2cf79fb5159db98cb2d4c61a9
michael <michaelsanchez8815@gmail.com>
2023-02-14 13:59:51 -0600

    loops 1 average

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,28 +1,60 @@
-﻿namespace HelloWorld;
+﻿using System.Diagnostics.Metrics;
+
+namespace HelloWorld;
 
 public class Exercise
 {
 
     static void Main(string[] args)
     {
+        bool continueLoop = true;
+        int counter = 0;
+        float sum = 0;
 
-            int i = -10;
+        while (continueLoop)
+        {
+            Console.WriteLine("Please write a number");
+            Console.WriteLine("Please write -1 when you are done.");
+            string input = GetInput();
+            float newNum;
 
-            while (i < 10)
+
+            if (input == null || float.TryParse(input, out newNum) == false)
             {
-
-                // TODO
-                if (i % 3 == 0)
-                {
-                    i++;
-                    continue;
-                }
-                if (i % 6 == 0)
-                {
-                    break;
-                }
-                Console.WriteLine(i++);
+                Console.WriteLine("Please provide a number.");
+                continueLoop = true;
+                continue;
             }
+
+            float.TryParse(input, out newNum);
+
+            if (newNum == -1)
+            {
+                float average = sum / counter;
+                Console.WriteLine($"The average number is {average}");
+                continueLoop = false;
+                continue;
+            }
+            
+            if (newNum < 0 || newNum > 20)
+            {
+                Console.WriteLine("The number must be between 0 and 20.");
+                continueLoop = true;
+                continue;
+            }
+
+            sum += newNum;
+            counter++;
+            Console.WriteLine($"Sum is {sum}");
+            Console.WriteLine($"counter is {counter}");
+        }
+        
+    }
+
+    public static string GetInput()
+    {
+        return Console.ReadLine();
+        
     }
 
 }
