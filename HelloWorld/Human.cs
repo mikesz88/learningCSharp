397441228d09c405bef857650100e5082c75b561
michael <michaelsanchez8815@gmail.com>
2023-02-17 11:13:15 -0600

    Creating a class and using public variable and methods

---
+++ HelloWorld/Human.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesBasics
+{
+    // this class is a blueprint for a datatype
+    class Human
+    {
+        // member variable
+        public string firstName;
+
+        // member
+        public void IntroduceMyself()
+        {
+            Console.WriteLine($"Hi, I'm {firstName}");
+        }
+    }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,46 +1,25 @@
-﻿using System.Diagnostics.Metrics;
+﻿using ClassesBasics;
+using System.Diagnostics.Metrics;
 
-namespace HelloWorld;
+namespace Classes___Basics;
 
-public class Exercise
+public class Program
 {
 
     static void Main(string[] args)
     {
-        string input = "0";
-        int count = 0;
-        int total = 0;
-        int currentNumber = 0;
+        // Create an object of my class
+        // an instance of Human
+        Human michael = new Human();
+        // access public variable from outside, and even change it
+        michael.firstName = "Michael";
+        // call methods of the class
+        michael.IntroduceMyself();
 
-        while (input != "-1")
-        {
-            Console.WriteLine($"Last number was {currentNumber}");
-            Console.WriteLine("Please enter the next score");
-            Console.WriteLine($"Current amount of entries: {count}");
-            Console.WriteLine("Please -1 once you are ready to calculate the average.");
-            
-            input = Console.ReadLine();
-            if (input.Equals("-1"))
-            {
-                Console.WriteLine("----------------------------------------------------------");
-                double average = (double)total / (double)count;
-                Console.WriteLine($"The average score of your students is {average}");
-            }
-            if (int.TryParse(input, out currentNumber) && currentNumber > 0 && currentNumber < 21)
-            {
-                total += currentNumber;
-            } else
-            {
-                if (!input.Equals("-1"))
-                {
-                    Console.WriteLine("Please enter a value between 1 and 20!");
+        Human lisa = new Human();
+        lisa.IntroduceMyself();
 
-                }
-                continue;
-            }
-            count++;
-        }
-        Console.ReadLine();
+        Console.ReadKey();
     }
 
 }
